50b92c30183e515da852556974739cccfb2b2223
Ivan Khanevich <mail@iamkhan.tech>
2023-06-27 13:01:07 +0400

    refactor(compose, ci): improve error message, change branch for ci

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,16 @@ export function compose<T extends any[], R>(
   const funcs = functions.filter((f) => typeof f === "function");
 
   if (funcs.length === 0) {
-    throw new Error("Must have at least one function");
+    throw new Error("Must have at least one function as argument");
   }
 
   return function (this: any, ...args: T): R {
     let result = funcs.at(-1)?.call(this, ...args);
+
     for (let i = funcs.length - 2; i >= 0; i--) {
       result = funcs[i].call(this, result);
     }
+
     return result as R;
   };
 }
