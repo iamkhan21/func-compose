2955d1d9b3e048ed25d1459f8fe4ef90d8e607e1
Ivan Khanevich <mail@iamkhan.tech>
2024-11-24 23:23:35 +0400

    BREAKING CHANGE: replace eslint and prettier with Biome and maintain dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 type Func<T extends any[], R> = (...a: T) => R;
 
 export function compose<R>(
-  f1: Func<any, R>,
-  ...functions: Func<any, any>[]
+	f1: Func<any, R>,
+	...functions: Func<any, any>[]
 ): Func<any, R> {
-  const funcs = [f1, ...functions].filter((f) => typeof f === "function");
+	const funcs = [f1, ...functions].filter((f) => typeof f === "function");
 
-  if (funcs.length === 0) {
-    throw new Error("Must have at least one function as argument");
-  }
+	if (funcs.length === 0) {
+		throw new Error("Must have at least one function as argument");
+	}
 
-  return function (this: any, ...args: any[]) {
-    let result = funcs.at(-1)?.call(this, ...args);
+	return function (this: any, ...args: any[]) {
+		let result = funcs.at(-1)?.call(this, ...args);
 
-    for (let i = funcs.length - 2; i >= 0; i--) {
-      result = funcs[i].call(this, result);
-    }
+		for (let i = funcs.length - 2; i >= 0; i--) {
+			result = funcs[i].call(this, result);
+		}
 
-    return result as R;
-  };
+		return result as R;
+	};
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -2,78 +2,76 @@ import { describe, expect, it, vi } from "vitest";
 import { compose } from "../src";
 
 describe("func-compose", () => {
-  it("should execute subfunctions", () => {
-    const func1 = vi.fn();
+	it("should execute subfunctions", () => {
+		const func1 = vi.fn();
 
-    const composed = compose(func1);
+		const composed = compose(func1);
 
-    composed(2);
+		composed(2);
 
-    expect(func1).toBeCalledWith(2);
-  });
+		expect(func1).toBeCalledWith(2);
+	});
 
-  it("should execute subfunctions in reverse order", () => {
-    const func1 = vi.fn((x) => x + 1);
-    const func2 = vi.fn((x) => x + 2);
-    const func3 = vi.fn((x) => x + 3);
+	it("should execute subfunctions in reverse order", () => {
+		const func1 = vi.fn((x) => x + 1);
+		const func2 = vi.fn((x) => x + 2);
+		const func3 = vi.fn((x) => x + 3);
 
-    const composed = compose(func1, func2, func3);
+		const composed = compose(func1, func2, func3);
 
-    const result = composed(2);
+		const result = composed(2);
 
-    expect(result).toBe(8);
+		expect(result).toBe(8);
 
-    expect(func3).toBeCalledWith(2);
-    expect(func2).toBeCalledWith(5);
-    expect(func1).toBeCalledWith(7);
-  });
+		expect(func3).toBeCalledWith(2);
+		expect(func2).toBeCalledWith(5);
+		expect(func1).toBeCalledWith(7);
+	});
 
-  it("should throw error if no functions are passed", () => {
-    // @ts-ignore
-    expect(() => compose()).toThrow(
-      "Must have at least one function as argument"
-    );
-  });
+	it("should throw error if no functions are passed", () => {
+		// @ts-ignore
+		expect(() => compose()).toThrow(
+			"Must have at least one function as argument",
+		);
+	});
 
-  it("should ignore non-functions", () => {
-    const func1 = vi.fn((x) => x + 1);
-    const func2 = vi.fn((x) => x + 2);
-    const func3 = vi.fn((x) => x + 3);
+	it("should ignore non-functions", () => {
+		const func1 = vi.fn((x) => x + 1);
+		const func2 = vi.fn((x) => x + 2);
+		const func3 = vi.fn((x) => x + 3);
 
-    const composed = compose(
-      func1,
-      func2,
-      func3,
-      // @ts-ignore
-      "1",
-      2,
-      false,
-      undefined,
-      {},
-      []
-    );
+		const composed = compose(
+			func1,
+			func2,
+			func3,
+			// @ts-ignore
+			"1",
+			2,
+			false,
+			undefined,
+			{},
+			[],
+		);
 
-    const result = composed(2);
+		const result = composed(2);
 
-    expect(result).toBe(8);
-  });
+		expect(result).toBe(8);
+	});
 
-  it("compose handles this correctly", () => {
-    const obj = {
-      value: "Hello, world!",
-      fn1: function () {
-        return this.value;
-      },
-      fn2: function (value: string) {
-        return value.toUpperCase();
-      },
-    };
+	it("compose handles this correctly", () => {
+		const obj = {
+			value: "Hello, world!",
+			fn1: function () {
+				return this.value;
+			},
+			fn2: (value: string) => value.toUpperCase(),
+		};
 
-    const composed = compose(obj.fn2, obj.fn1);
+		const composed = compose(obj.fn2, obj.fn1);
 
-    // When called as a method on obj, composed should return obj.value.toUpperCase()
-    const result = composed.call(obj);
+		// When called as a method on obj, composed should return obj.value.toUpperCase()
+		const result = composed.call(obj);
 
-    expect(result).toBe("HELLO, WORLD!");
-  });
+		expect(result).toBe("HELLO, WORLD!");
+	});
 });
